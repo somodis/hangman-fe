4a08e9bae0b1f7208403fd2a0268863296a81dfb
Fanni Somodi <fanni.somodi@attrecto.com>
2023-05-05 14:25:36 +0200

    WIP: auth

---
+++ src/models/credentials.model.ts
@@ -0,0 +1,8 @@
+export interface LoginCredentials {
+  username: string;
+  password: string;
+}
+
+export interface TokenResponse {
+  token: string;
+}
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,2 +1,11 @@
-class AuthService {}
+import request, { Methods } from '../utils/request';
+import { LoginCredentials, TokenResponse } from '../models/credentials.model';
+
+class AuthService {
+    async login(data: LoginCredentials) {
+        return request<TokenResponse>({ resource: 'auth/login', data, method: Methods.POST });
+    }
+}
 export const authService = new AuthService();
+
+
